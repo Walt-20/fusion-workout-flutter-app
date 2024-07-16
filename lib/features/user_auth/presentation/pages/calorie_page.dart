76e3b45beeb132609a10c52b6b81ac39398f0789
Walt <walt.c0des20@gmail.com>
2024-07-16 11:30:32 -0600

    WOA-31 created calorie tracking branch

---
+++ lib/features/user_auth/presentation/pages/calorie_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalorieTrackingPage extends StatefulWidget {
+  const CalorieTrackingPage({super.key});
+
+  @override
+  State<CalorieTrackingPage> createState() => _CalorieTrackingPageState();
+}
+
+class _CalorieTrackingPageState extends State<CalorieTrackingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
